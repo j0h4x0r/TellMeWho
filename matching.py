9202badd1a3c3b89a19da5f170ad484ceba6ba01
j0h4x0r <troyal.lee@gmail.com>
2015-03-25 18:05:12 -0400

    -Add valid type list

---
+++ matching.py
@@ -0,0 +1,11 @@
+accepted_type_list = [
+	'/people/person': 'Person',
+	'/book/author': 'Author',
+	'/flim/actor': 'Actor',
+	'/tv/tv_actor': 'Actor',
+	'/organization/organization_founder', 'BusinessPerson',
+	'/business/board_membber': 'BusinessPerson',
+	'/sports/sports_league': 'League',
+	'/sports/sports_team': 'SportsTeam',
+	'/sports/professional_sports_team': 'SportsTeam'
+]
