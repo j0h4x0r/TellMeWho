ab8827b4de3507cedd3ec268b9eebe8300e779c0
j0h4x0r <troyal.lee@gmail.com>
2015-03-26 00:24:38 -0400

    Add core function of question answering. Change accepted_type_list to OrderedDict

---
+++ answer.py
@@ -0,0 +1,52 @@
+import urllib, json
+
+api_key = 'AIzaSyDMaf8g5AnI_OI7jR3ck5VVR2tf8LWmhQg'
+mqlread_url = 'https://www.googleapis.com/freebase/v1/mqlread'
+
+def main():
+	MQLquery('Microsoft', 'BusinessPerson')
+
+def MQLquery(x, ans_type):
+	# build query
+	if ans_type == 'Author':
+		ans_point = "/book/author"
+		query_point = "/book/author/works_written"
+	elif ans_type == 'BusinessPerson':
+		ans_point = "/organization/organization_founder"
+		query_point = "/organization/organization_founder/organizations_founded"
+	else:
+		return []
+	
+	# qurey API
+	query = [{
+		query_point: [{
+			"a:name": None,
+			"name~=": x
+		}],
+		"id": None,
+		"name": None,
+		"type": ans_point
+	}]
+	params = {
+		'query' : query,
+		'key': api_key
+	}
+	url = mqlread_url + '?' + urllib.urlencode(params).replace('None', 'null').replace('%27', '%22')
+	response = json.loads(urllib.urlopen(url).read())['result']
+
+	# parse response
+	result = []
+	for item in response:
+		ans = item['name'] + '(as ' + ans_type + ') created '
+		fullname_len = len(item[query_point])
+		for i in range(fullname_len):
+			if i == 0:
+				ans += item[query_point][i]['a:name']
+			elif i == fullname_len - 1:
+				ans += ' and ' + item[query_point][i]['a:name'] + '.'
+			else:
+				ans += ', ' + item[query_point][i]['a:name']
+		result.append(ans)
+	print result
+
+if __name__ == '__main__': main()
